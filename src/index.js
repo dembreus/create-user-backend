ebccefc505b8c63f68c81adb79bbf7bfc3cadf45
Dillon <dillon.embreus@gmail.com>
2018-10-10 14:51:53 -0600

    updated PUT

--- src/index.js
+++ src/index.js
@@ -1,43 +1,57 @@
-import {send, json} from 'micro'
-import {router, get, post, put, del} from 'microrouter'
+import { send, json } from "micro";
+import { router, get, post, put, del } from "microrouter";
 // import Users from './db/users'
 const cors = require("micro-cors")();
-const db = require('monk')('mongodb://blue5:5blue5@ds018498.mlab.com:18498/create-user-backend');
-const users = db.get('users');
+const db = require("monk")(
+  "mongodb://blue5:5blue5@ds018498.mlab.com:18498/create-user-backend"
+);
+const users = db.get("users");
 
-export default cors(router(
-    get('/', async (req, res) => {
-        const results = await users.find({});
-        await send(res, 200, results)
+export default cors(
+  router(
+    get("/", async (req, res) => {
+      const results = await users.find({});
+      await send(res, 200, results);
     }),
-    get('/:id', async (req, res) => {
-        const _id = req.params.id;
-        const user = await users.findOne({ _id });
+    get("/:id", async (req, res) => {
+      const _id = req.params.id;
+      const user = await users.findOne({ _id });
 
-        if(!user) {
-            return send(res, 404, { message: `Figure not found ${_id}`})
-        }
-        return send(res, 200, user)
+      if (!user) {
+        return send(res, 404, { message: `Figure not found ${_id}` });
+      }
+      return send(res, 200, user);
     }),
-    post('/', async (req, res) => {
-        const user = await json(req);
-        const result = await users.insert(user);
-        return send(res, 201, result)
+    post("/", async (req, res) => {
+      const user = await json(req);
+      const result = await users.insert(user);
+      return send(res, 201, result);
     }),
-    put('/:id', async (req, res) => {
-        const {firstName, lastName, email, phoneNumber, password, confirmPassword} = await json(req);
-        const id = req.params.id;
-        const result = await users.findOneAndUpdate({_id: id}, {firstName, lastName, email, phoneNumber, password, confirmPassword});
-        console.log(result);
-        return send(res, 200, {ok: result})
+    put("/:id", async (req, res) => {
+      const { id } = req.params;
+      const {
+        firstName,
+        lastName,
+        email,
+        phoneNumber,
+        password,
+        confirmPassword
+      } = await json(req);
+      const result = await users.findOneAndUpdate(
+        { _id: id },
+        { firstName, lastName, email, phoneNumber, password, confirmPassword }
+      );
+      console.log(result);
+      return send(res, 200, result);
     }),
-    del('/:id', async (req, res) => {
-        const id = req.params.id;
-        const deleted = await users.remove({_id: id});
-        const find = await users.find({});
-        console.log(deleted);
-        console.log(find);
-        return send(res, 200, {})
+    del("/:id", async (req, res) => {
+      const id = req.params.id;
+      const deleted = await users.remove({ _id: id });
+      const find = await users.find({});
+      console.log(deleted);
+      console.log(find);
+      return send(res, 200, {});
     }),
-    get('/*', (req, res) => send(res, 404, 'Not found route')),
-))
+    get("/*", (req, res) => send(res, 404, "Not found route"))
+  )
+);
